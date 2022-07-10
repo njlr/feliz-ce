676591a2b4f9e3d30b1ceab8d84eec34d0927af9
njlr <njlr@users.noreply.github.com>
2022-07-10 20:12:34 +0100

    * Adds applicative example

---
+++ examples/two-counters/App.fs
@@ -0,0 +1,31 @@
+﻿module App
+
+open Feliz
+open Feliz.CE
+
+let counter initialCount =
+  react {
+    let! count1, setCount1 = React.withState 0
+    and! count2, setCount2 = React.withState 0
+
+    return
+      Html.div
+        [
+          Html.h1 $"Count 1: {count1}"
+          Html.button
+            [
+              prop.text "Increment"
+              prop.onClick (fun _ -> setCount1 (count1 + 1))
+            ]
+          Html.h1 $"Count 2: {count2}"
+          Html.button
+            [
+              prop.text "Increment"
+              prop.onClick (fun _ -> setCount2 (count2 + 1))
+            ]
+        ]
+  }
+
+open Browser.Dom
+
+ReactDOM.render(counter 0, document.getElementById "root")
